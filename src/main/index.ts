e323b9a6d848e246bda5a0847fa1d7429906f410
Annt <annguyenthe.work@gmail.com>
2024-02-18 16:10:42 +0700

    setup base layout template

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,6 +1,6 @@
-import { app, shell, BrowserWindow, ipcMain } from 'electron'
+import { electronApp, is, optimizer } from '@electron-toolkit/utils'
+import { BrowserWindow, app, ipcMain, shell } from 'electron'
 import { join } from 'path'
-import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
 
 function createWindow(): void {
@@ -11,6 +11,15 @@ function createWindow(): void {
     show: false,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
+    // My settings
+    center: true, // set window display center
+    title: 'NoteMark',
+    frame: false,
+    vibrancy: 'under-window',
+    visualEffectState: 'active',
+    titleBarStyle: 'hidden',
+    trafficLightPosition: { x: 15, y: 10 },
+    // End My settings
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
       sandbox: true,
